dc467342a1be6a4e72291f2a22997b606fe5646e
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-05-15 19:07:41 +0800

    update

---
+++ src/git/isInGitRepository/index.ts
@@ -0,0 +1,10 @@
+import { execSync } from 'child_process';
+
+function isInGitRepository() {
+  try {
+    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
